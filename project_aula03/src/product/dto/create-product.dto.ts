712ba9d6e646aa9800100de2e60e8ed0cbc70891
raimundlima <77973887+raimundlima@users.noreply.github.com>
2021-09-27 13:36:34 -0300

    crud parte II

--- project_aula03/src/product/dto/create-product.dto.ts
+++ project_aula03/src/product/dto/create-product.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductDto {}
+import { Prisma } from "@prisma/client";
+import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Product } from "../entities/product.entity";
+
+export class CreateProductDto extends Product {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsInt()
+    @IsOptional()
+    price?: number | null;
+    
+    
+    images?: Prisma.ImageUncheckedCreateNestedManyWithoutProductInput;
+
+}
